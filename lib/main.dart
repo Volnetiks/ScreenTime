a382fa440016bc4a1c37a0a57922bd88f8c4e8fd
Volnetiks <volnetiks@gmail.com>
2022-05-09 17:00:41 +0200

    Created basic model of the UI

--- lib/main.dart
+++ lib/main.dart
@@ -41,20 +41,46 @@ class _ScreenTimePageState extends State<ScreenTimePage> {
         title: Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text("Screen Time",
+            children: [
+              const Text("Screen Time",
                   style: TextStyle(
                       color: Colors.black, fontWeight: FontWeight.w500)),
               Text("Last Week",
-                  style: TextStyle(color: Colors.purple, fontSize: 14)),
+                  style:
+                      TextStyle(color: Colors.purple.shade400, fontSize: 14)),
             ]),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Container(
+              height: 225,
+              color: Colors.white,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(top: 15, left: 20, bottom: 15),
+                    child: Text("2h 15min",
+                        style: TextStyle(color: Colors.black, fontSize: 20)),
+                  )
+                ],
+              )),
           const SizedBox(
-            height: 10,
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Most used",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("Show Categories",
+                    style: TextStyle(color: Colors.purple.shade400))
+              ],
+            ),
           ),
           Expanded(
             child: Padding(
